27ac4b8b88f841ed2daacbf0c43bc70c53c59642
PhucAn04 <pham27282@gmail.com>
2025-06-15 22:10:19 +0700

    first demo controller

---
+++ DemoController/DemoController/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using DemoController.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoController.Controllers
+{
+    public class BookController : Controller
+    {
+        static List<Book> books = new List<Book>()
+        {
+            new Book { BookId = 1, Title = "Thiết kế Web", Author = "HIENLTH", Price = 99000, IsExpire = false },
+            new Book { BookId = 1, Title = "Lập trình ASP.NET Core", Author = "HIENLTH", Price = 159000, IsExpire = false },
+        };
+
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            //Validate the book object
+            books.Add(book);
+            return RedirectToAction("Index");
+        }
+
+        //public IActionResult Create(Book book)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        books.Add(book);
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(book);
+        //}
+    }
+}
---
+++ DemoController/DemoController/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoController.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult ActionIndex()
+        {
+            ViewBag.MyData = "Hello from ActionIndex!";
+            return View("MyView");
+        }
+
+        public ViewResult MyView()
+        {
+            ViewBag.MyData = "Hello from MyView!";
+            return View();
+        }
+
+        public string ActionTest()
+        {
+            return "Hello world!";
+        }
+
+        //public IActionResult AAA()
+        //{
+        //    return Content("OK");
+        //}
+
+        public int BBB()
+        {
+            return new Random().Next();
+        }
+    }
+}
